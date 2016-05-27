c1fbc2982c1098df53b2339587417892506f380a
Paul Nunez <paulnunezm@gmail.com>
2016-05-27 00:16:11 -0400

    added progress while chart changes

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/HistoricalActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/HistoricalActivity.java
@@ -167,18 +167,21 @@ HistoricalActivity extends AppCompatActivity implements Callback,
 
   private void hideLoading() {
     mContent.setVisibility(View.VISIBLE);
+    hideChartLoading();
+  }
+
+  private void hideChartLoading(){
+    linechart.setVisibility(View.VISIBLE);
+  }
+
+  private void showChartLoading(){
+    linechart.setVisibility(View.INVISIBLE);
   }
 
   private void showErrorMessage() {
   }
 
   private void setRangeOnClickListeners() {
-//    mOneDaySelector.setOnClickListener(this);
-//    mOneMonthSelector.setOnClickListener(this);
-//    mTwoMonthSelector.setOnClickListener(this);
-//    mFourMonthSelector.setOnClickListener(this);
-//    mSixMonthSelector.setOnClickListener(this);
-
     for(int i = 0; i < chartRange.size(); i++){
       chartRange.get(i).setOnClickListener(HistoricalActivity.this);
     }
@@ -209,8 +212,6 @@ HistoricalActivity extends AppCompatActivity implements Callback,
         "format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org" +
         "%2Falltableswithkeys&callback=";
 
-    // url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22GOOGL%22%20and%20startDate%20%3D%20%222015-09-11%22%20and%20endDate%20%3D%20%222016-03-10%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
-//    url = URLEncoder.encode(url);
     return url;
   }
 
@@ -343,6 +344,8 @@ HistoricalActivity extends AppCompatActivity implements Callback,
   public void onClick(View v) {
 
     DateTime startDate;
+
+    showChartLoading();
 
     try {
 
